c19997400a468c9bdd2c2215c178b0eedbfac495
Christopher Jacoby <christopher.a.jacoby@gmail.com>
2022-11-05 18:35:54 -0500

    initial commit

---
+++ db/migrate/20221103145826_create_tasks.rb
@@ -0,0 +1,13 @@
+class CreateTasks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.string :room
+      t.string :assigned
+      t.integer :allowance
+      t.boolean :complete
+
+      t.timestamps
+    end
+  end
+end
