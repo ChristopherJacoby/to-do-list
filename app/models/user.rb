c6e5ebe587228a2cddfe783817369a4ba99aa505
Christopher Jacoby <christopher.a.jacoby@gmail.com>
2022-11-24 00:01:49 -0600

    added username to app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-  validates :name, presence: true
+  validates :name, :username, presence: true, uniqueness: {case_sensitive: false }
   has_secure_password
   validates :email, email: true, uniqueness: {case_sensitive: false}
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_21_225013) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_24_054148) do
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.string "room"
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_225013) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
   end
 
 end
