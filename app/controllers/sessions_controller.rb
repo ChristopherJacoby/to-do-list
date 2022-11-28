b8b7a1ebf6fa8d6f2a3dfc06e5239e9f81dae232
Christopher Jacoby <christopher.a.jacoby@gmail.com>
2022-11-28 12:44:25 -0600

    created sign out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,21 @@ class SessionsController < ApplicationController
 
   def new; end
 
-  def create; end
+  def create
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user, notice: "Welcome back, #{user.name}!"
+    else
+      flash.now[:alert] = "Invalid email/password combination!"
+      render :new, status: :unprocessable_entity
+    end
+  end
 
-  def destroy; end
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, status: :see_other,
+    notice: "You have signed out."
+  end
 
 end
